4eb635c02af3e245ef6da980c67390a73f443355
LuisGoCas <LuisGoCas@users.noreply.github.com>
2014-12-26 18:03:47 -0500

    Create a Board.h

---
+++ Board.h
@@ -0,0 +1,18 @@
+#pragma once
+class Board
+{
+private:
+	int boardWide;
+	int boardHigh;
+	char Data[20][20];
+public:
+	Board();
+	void setBoardSize(int wide, int high);
+	int getBoardWide();
+	int getBoardHigh();
+	char getBoardData(int x, int y);
+	void initBoard();
+	void printBoard();
+	bool modifyBoard(int x, int y, char c);
+	bool hasWon(int markersWin, char playerSymb);
+};
